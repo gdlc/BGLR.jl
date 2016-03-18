c3e6798609502c1af43e8c4ad96adada3f3ea0d6
Gustavo de los Campos <gdeloscampos@gmail.com>
2016-03-18 13:53:10 -0400

    add scale(X,scale=true,center=true)

--- src/BGLR.jl
+++ src/BGLR.jl
@@ -815,3 +815,17 @@ end
 # test
 
 end #module end
+
+
+### UTILS
+
+function scale(Array{Number,2};center=true,scale=true)
+    n,p=size(X)
+    for j in 1:p
+        xj=X[:,j]
+        mu=mean(xj)
+        SD=std(xj)
+    	X[:,j]=(xj-mu)/SD
+    end
+	X
+end
