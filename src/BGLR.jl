5dfbcdf5a2dd6dadcffae56a177396a1f0045402
Gustavo de los Campos <gdeloscampos@gmail.com>
2016-03-18 14:01:16 -0400

    Update BGLR.jl

--- src/BGLR.jl
+++ src/BGLR.jl
@@ -819,15 +819,13 @@ end #module end
 
 ### UTILS
 
-#function scale(X::Array{Number,2};center=true,scale=true)
- #   n,p=size(X)
- #   for j in 1:p
- #       xj=X[:,j]
- #       mu=mean(xj)
- #       SD=std(xj)
- #   	X[:,j]=(xj-mu)/SD
- #   end
-
-#	X
-
-#end
+function scale(X::Array{Float64,2};center=true,scale=true)
+    n,p=size(X)
+    for j in 1:p
+        xj=X[:,j]
+        mu=mean(xj)
+        SD=std(xj)
+    	X[:,j]=(xj-mu)/SD
+    end
+	X
+end
